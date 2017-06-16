49d5734fe73d6c6a447142d7f68a07f553195443
Aizpea Babaze <aizpea_babaze@hotmail.com>
2017-06-16 10:51:21 +0200

    bidea eskubian

---
+++ demos/accessible/funtzioak.js
@@ -0,0 +1,54 @@
+function move_forward2(){
+  setTimeout(function(){
+    document.getElementById("emaitza").innerHTML = "";
+  }, time);
+  switch (g) {
+    case 1:
+      var element=document.getElementById("minijokoaTable").rows[i].cells[j+1].innerHTML;
+      if(element=="B"){
+        //Bidea dagoela esan nahi du.
+        document.getElementById("minijokoaTable").rows[i].cells[j].innerHTML="B";
+        j++;
+        document.getElementById("minijokoaTable").rows[i].cells[j].innerHTML="MB";
+        document.getElementById("emaitza").innerHTML = "Lauki bat mugitu da aurrerantz";
+      }else if(element=="H"){
+        //Helmugara iritsi da.
+        //TODO: Bukatu
+      }else //TODO: ERROREA
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    default:
+
+  }
+  document.getElementById("emaitza").setAttribute("role", "alert");
+}
+function move_forward(){
+  setTimeout(function(){
+    move_forward2();
+  }, time);
+  time=time+3000;
+}
+function bidea_eskubian(){
+  switch (g) {
+    case 1:
+      return document.getElementById("minijokoaTable").rows[i+1].cells[j].innerHTML=="B";
+      break;
+    case 2:
+      return document.getElementById("minijokoaTable").rows[i].cells[j+1].innerHTML=="B";
+      break;
+    case 3:
+      return document.getElementById("minijokoaTable").rows[i-1].cells[j].innerHTML=="B";
+      break;
+    case 4:
+      return document.getElementById("minijokoaTable").rows[i].cells[j-1].innerHTML=="B";
+      break;
+    default:
+
+  }
+  //return document.getElementById("minijokoaTable").rows[i].cells[j+1].innerHTML=="B";
+}
